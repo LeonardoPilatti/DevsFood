f7ea1efa41b73ae0807dc4488450c76c5019ee02
Leonardo Pilatti <leonardopilatti14@gmail.com>
2021-06-18 19:41:46 -0300

    criando o arquivo da api e pegando a lista de categorias

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { Container } from './styled';
 
+import api from '../../api';
+
 import Header from '../../components/Header';
 
 export default () => {
   const history = useHistory();
   const [headerSearch, setHeaderSearch] = React.useState('');
+
+  React.useEffect(() => {
+    const getCategories = async () => {
+      const categories = await api.getCategories();
+      console.log(categories);
+    };
+    getCategories();
+  }, []);
 
   return (
     <Container>
